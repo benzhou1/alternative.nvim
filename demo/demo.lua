91c1e7ac42cf529fd1c6c12bf30fbd3a36ad4d42
goose <purification1997@gmail.com>
2025-01-15 20:41:15 +0700

    chore: add demo file

---
+++ demo/demo.lua
@@ -0,0 +1,14 @@
+-- general.boolean_flip: convert true to false and vice versa
+local flag = true
+
+-- general.number_increment_decrement: increment or decrement the number
+local number = 1
+
+-- lua.if_condition_flip: flip the condition in the if statement.
+if flag then
+  print("true")
+end
+
+-- lua.ternary_to_if_else: convert ternary expression to if else statement.
+-- Preview the change before applying it.
+local foo = a and b or c
